12a51ad66ddd9e9fd82895f705a6e596c2f05e5c
Patricia Santana <psantana.student@hsfi.us>
2017-05-02 20:45:17 +0000

    the api works now!

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-$(document).ready(function(){
-     function initialize() {
-  var mapOptions = {
-     voom: 8,
-     center: new google.maps.LatLng
-            (40.741992,-73.927947)
-    };
-  var map = new google.maps.map(
-      document.get.ElementById('map-canvas'),
-      mapOptions);
+
+    function initialize() {
+        var mapOptions = {
+            zoom: 8,
+            center: new google.maps.LatLng(40.741992, -73.927947)
+        };
+        var map = new google.maps.Map(
+            document.getElementById('map-canvas'),
+            mapOptions);
     }
-     function loadScript() {
- var script = document.createElement('script')
-     script.type = 'text/javascript';
-     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' + 
-                  'callback=initialize';
-     script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBUBbDo50tZlJK9s_ODHVGGNjodukds0OQ'
-    document.body.appendChild(script);
-    }
-window.onload = loadScript;
-    });
+
+
