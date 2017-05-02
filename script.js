b85ff15c981af59518f6ef2d8ae998a3531d8dd7
Patricia Santana <psantana.student@hsfi.us>
2017-05-02 21:33:37 +0000

    started object with all brooklyn schools

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
+function initialize() {
+    var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(40.741992, -73.927947)
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var myLatlng= {lat: 40.741992, lng: -73.927947};
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        title: "Queens Vocational High School"
+    });
+    marker.setMap(map);
+}
+var brooklynNames = ["Academy of Innovative Technology", "All City Leadership", "Brooklyn School for Law and Technology", "Brooklyn Institute for Liberal Art", 
+"Brooklyn International High School", "City Polytechnic High School" ];
+var brooklynLatLng = {lat: 40.6928500, lng: -73.8689910, lat: 40.6975910, lng: -73.9130970, lat: 40.6884530, lng: -73.9207640, lat: 40.6586120, lng: -73.9430920, lat: 40.6973450, lng: -73.9844960, lat: 40.6956550, lng: -73.9858310};
 
-    function initialize() {
-        var mapOptions = {
-            zoom: 8,
-            center: new google.maps.LatLng(40.741992, -73.927947)
-        };
-        var map = new google.maps.Map(
-            document.getElementById('map-canvas'),
-            mapOptions);
+for  (i=0; i<brooklynNames.length; i++ ){
+
+
+]
+
+
+
+var brooklynSchools = [
+    
+    {
+        title:"Academy of Innovative Technology" ,
+        lat:40.6928500 ,
+        lng:-73.8689910
     }
-
-
+    
+    {
+        title:"All City Leadership"
+        lat:40.6975910
+        lng:
+    }
+    
+    {
+        title:
+        lat:
+        lng:
+    }
+    
+    {
+        title:
+        lat:
+        lng:
+    }
+    
+    {
+        title:
+        lat:
+        lng:
+    }
+    
+    ]
